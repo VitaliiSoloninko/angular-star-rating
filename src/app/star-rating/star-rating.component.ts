60a07d8ac37046524c7b41970a97dd4b6e8be3fd
22051988Di <vitalii.soloninko@gmail.com>
2024-12-05 13:58:02 +0100

    1.1	Star rating with size and dark mode

--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgFor, NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star-rating',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgStyle],
   templateUrl: './star-rating.component.html',
-  styleUrl: './star-rating.component.scss'
+  styleUrl: './star-rating.component.scss',
 })
 export class StarRatingComponent {
+  @Input()
+  stars: number = 0;
 
+  @Input()
+  size: number = 1;
+
+  get styles() {
+    return {
+      'width.rem': this.size,
+      'height.rem': this.size,
+      'marginRight.rem': this.size / 6,
+    };
+  }
+
+  getStarImage(current: number): string {
+    const previousHaft = current - 0.5;
+    const imageName =
+      this.stars >= current
+        ? 'star-full'
+        : this.stars >= previousHaft
+        ? 'star-haft'
+        : 'star-empty';
+    return `/stars/${imageName}.svg`;
+  }
 }
